c534bb7d958b8eca77707db8cfa811b72a2bbdd8
Vitormarden <vitomarden1234@gmail.com>
2022-10-05 16:42:16 -0300

    criando Api de Inventory

---
+++ Inventory/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Inventory.Models;
+using Inventory.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase   
+    {
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetAll();
+            
+        }
+        [HttpGet("{id}")]
+        public Product GetById(int id)
+        {
+           return ProductService.Get(id);
+        }
+
+        [HttpPost]
+        public void Post(Product product)
+        {
+            ProductService.Add(product);
+        }
+        [HttpPut]
+        public void Put(Product product)
+        {
+            ProductService.Update(product);
+        }
+        [HttpDelete("{id}")] 
+        public void Delete(int id )
+        {
+            ProductService.Delete(id);
+        }
+    }
+}
---
+++ Inventory/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Inventory.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+   
+}
